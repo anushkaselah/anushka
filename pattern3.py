4589c9a9b0d7183280307420e4e49789c5f0ac8b
anushkaselah <61589118+anushkaselah@users.noreply.github.com>
2020-03-24 16:08:18 +0530

    Update pattern3.py

--- pattern3.py
+++ pattern3.py
@@ -5,6 +5,6 @@ for i in range(0,n):
     for j in range(0,k):
         print(end="")
         k=k-1
-         for j in range(0,i+1):
+     for j in range(0,i+1):
           print("*",end="")                      
-           print("\r")        
+      print("\r")        
